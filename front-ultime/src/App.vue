9bb4c30ad3e7c617512339a4f849f10261bf844a
juanmgarcia97 <juanmar-p@hotmail.com>
2020-11-23 13:08:51 -0500

    Planner and table for description task added

--- front-ultime/src/App.vue
+++ front-ultime/src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Planner/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Planner from './components/Planner';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Planner,
   },
 
   data: () => ({
