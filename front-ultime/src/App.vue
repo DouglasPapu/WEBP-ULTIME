346879167fe7ffff313f0bd077fc2393ad04623a
DouglasPapu <douglas.lopez1@correo.icesi.edu.co>
2020-11-25 10:32:17 -0500

    VueRouter added and initial schedule, also Home was added

--- front-ultime/src/App.vue
+++ front-ultime/src/App.vue
@@ -1,60 +1,28 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+  <v-app id="app">
+    <main>
+      <div class="app-container">
+        <header class="app-header">
+          <!--<NavBar></NavBar>-->
+        </header>
+        <div class="content">
+          <router-view></router-view>
+        </div>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <Planner/>
-    </v-main>
+    </main>
   </v-app>
 </template>
 
 <script>
-import Planner from './components/Planner';
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-    Planner,
-  },
+  components: {},
 
   data: () => ({
     //
   }),
 };
 </script>
+<style>
+</style>
--- front-ultime/src/main.js
+++ front-ultime/src/main.js
@@ -1,10 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
+import Planner from "./components/Planner";
+import Schedule from "./components/Schedule";
+import Home from "./components/Home";
 
-Vue.config.productionTip = false
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/planner",
+    component: Planner,
+  },
+  {
+    path: "/schedule",
+    component: Schedule,
+  },
+];
+
+Vue.config.productionTip = false;
+
+const router = new VueRouter({ routes });
+
+Vue.use(VueRouter);
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
