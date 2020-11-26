4e0ecbc5896473c93d2eee791cd3409a5d34b69a
Laura Rubio <laurarubio11@hotmail.com>
2020-11-26 13:05:13 -0500

    login and register user added

--- back-ultime/app.js
+++ back-ultime/app.js
@@ -17,11 +17,17 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('hoy es lunes'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 
--- back-ultime/controllers/userController.js
+++ back-ultime/controllers/userController.js
@@ -1,20 +1,45 @@
 const db = require('../config/posgres')
+var md5 = require('md5')
+
 
 exports.index = async (req, res, next) =>{
     const response = await db.query('SELECT * FROM public."User"')   
     res.send(response.rows);
 
   };
+
 exports.create =  async (req, res, next) =>{
-    var sql= 'INSERT INTO public."User" (lastname,firstname, passwd,username) VALUES ($1, $2, $3, $4)';
+  var sqlvalidate= 'SELECT * FROM public."User" WHERE username = $1';
+  var validate = db.query(sqlvalidate,[req.body.username])
+  console.log(validate)
     let user = {       
         lastname: req.body.lastname,
         firstname: req.body.firstname,
-        passwd: req.body.passwd,
+        passwd: md5(req.body.passwd),
         username: req.body.username       
-      }
-    
-    await db.query(sql,[user.lastname,user.firstname,user.passwd,user.username])   
-    res.send('User created');
+      }     
+    if(validate !=='Promise { <pending> }'){
+      var sql= 'INSERT INTO public."User" (lastname,firstname, passwd,username) VALUES ($1, $2, $3, $4)';
+      await db.query(sql,[user.lastname,user.firstname,user.passwd,user.username])   
+      res.send('User created');
+    }else{
+      res.send('The username already exists')
+    }
+   
 
   };
+
+ exports.login = async (req,res, nex)=> {
+ 
+   var username = req.body.username
+   var password = md5(req.body.passwd)  
+    var sql = 'SELECT * FROM public."User" WHERE username = $1';
+    const response = await db.query(sql, [username]);      
+    console.log("ENTRANDO",response.rows[0].username)
+    if (username === response.rows[0].username && password === response.rows[0].passwd){
+        return res.send('user loged')
+    } else{
+      return res.send('The users no exists');
+    }
+    
+};
