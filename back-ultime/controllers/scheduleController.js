3983b422d2bd3d5f62e1e361c33c6d0cf2f72037
DouglasPapu <douglas.lopez1@correo.icesi.edu.co>
2020-11-27 00:21:54 -0500

    method create subject almost ready

--- back-ultime/controllers/scheduleController.js
+++ back-ultime/controllers/scheduleController.js
@@ -3,10 +3,20 @@ const db = require("../config/posgres");
 exports.create = async (req, res, next) => {
   var sql = 'INSERT INTO public."Schedule" (fk_user,semester) VALUES ($1, $2)';
   let schedule = {
-    User_id: req.body.fk_user,
+    fk_user: req.body.fk_user,
     semester: req.body.semester,
   };
 
-  await db.query(sql, [schedule.User_id, schedule.semester]);
+  await db.query(sql, [schedule.fk_user, schedule.semester]);
   res.send("Schedule created");
+};
+
+exports.getByUser = async (req, res, next) => {
+  var sql = 'SELECT pk_schedule FROM public."Schedule" WHERE fk_user = $1';
+
+  let schedule = {
+    fk_user: req.body.fk_user,
+  };
+
+  await db.query(sql, [schedule.fk_user]);
 };
--- back-ultime/controllers/subjectController.js
+++ back-ultime/controllers/subjectController.js
@@ -1,22 +1,84 @@
+var moment = require("moment");
 const db = require("../config/posgres");
 
 exports.create = async (req, res, next) => {
-  var sql =
-    'INSERT INTO public."Subject" (fk_schedule,sub_name, start_time,end_time,sub_day) VALUES ($1, $2, $3, $4, $5)';
-  let subject = {
-    fk_schedule: req.body.fk_schedule,
-    sub_name: req.body.sub_name,
-    start_time: req.body.start_time,
-    end_time: req.body.end_time,
-    sub_day: req.body.sub_day,
+  var sql2 = 'SELECT pk_schedule FROM public."Schedule" WHERE fk_user = $1';
+  let schedule = {
+    fk_user: req.body.fk_user,
   };
 
-  await db.query(sql, [
+  var validate = await db.query(sql2, [schedule.fk_user]);
+  // Validation to get the schedule of the current user.
+  if (validate.rowCount !== 0) {
+    var sql3 = 'SELECT * FROM public."Subject" WHERE fk_schedule = $1';
+    let subject2 = {
+      fk_schedule: validate.rows[0].pk_schedule,
+    };
+
+    //Validation to get all the subjects programmed by current user
+    var validate2 = await db.query(sql3, [subject2.fk_schedule]);
+
+    var subject = {
+      fk_schedule: validate.rows[0].pk_schedule,
+      sub_name: req.body.sub_name,
+      start_time: req.body.start_time,
+      end_time: req.body.end_time,
+      sub_day: req.body.sub_day,
+    };
+    if (validate2.rowCount === 0) {
+      var sql =
+        'INSERT INTO public."Subject" (fk_schedule,sub_name, start_time,end_time,sub_day) VALUES ($1, $2, $3, $4, $5)';
+      await db.query(sql, [
+        subject.fk_schedule,
+        subject.sub_name,
+        subject.start_time,
+        subject.end_time,
+        subject.sub_day,
+      ]);
+      res.status(200).send("Subject created");
+    } else {
+      validate2.rows.forEach((element) => {
+        currentTime = moment(subject.start_time, "HH:mm");
+        currentFinal = moment(subject.end_time, "HH:mm");
+        startTime = moment(element.start_time, "HH:mm");
+        endTime = moment(element.end_time, "HH:mm");
+
+        console.log(currentTime.isBetween(startTime, endTime));
+        console.log(currentFinal.isBetween(startTime, endTime));
+        if (
+          (currentTime.isBetween(startTime, endTime) ||
+            currentFinal.isBetween(startTime, endTime)) &&
+          subject.sub_day == element.sub_day
+        ) {
+          res.status(406).send("This field is busy");
+        } else {
+          var sqlFinal =
+            'INSERT INTO public."Subject" (fk_schedule,sub_name, start_time,end_time,sub_day) VALUES ($1, $2, $3, $4, $5)';
+          db.query(sqlFinal, [
+            subject.fk_schedule,
+            subject.sub_name,
+            subject.start_time,
+            subject.end_time,
+            subject.sub_day,
+          ]);
+          res.status(200).send("Subject created");
+        }
+      });
+    }
+  } else {
+    res.status(406).send("The user doesn't exist");
+  }
+
+  //console.log(validate.rows[0].pk_schedule);
+  /*   await db.query(sql, [
     subject.fk_schedule,
     subject.sub_name,
     subject.start_time,
     subject.end_time,
     subject.sub_day,
-  ]);
-  res.send("Subject created");
+  ]); */
+};
+
+exports.get = async (req, res, next) => {
+  var sql = 'SELECT * FROM public."Subject"';
 };
